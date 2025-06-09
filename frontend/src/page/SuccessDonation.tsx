21bd742154853b5f10444ea1758f20eb3f6befce
lfn2000 <l13592367891@163.com>
2025-06-09 16:18:47 +0800

    update successdonation page

--- frontend/src/page/SuccessDonation.tsx
+++ frontend/src/page/SuccessDonation.tsx
@@ -4,19 +4,19 @@ import '../css/successdonation.css';
 const SuccessDonation = () => {
     return (
         <div className="success-page">
-            {/* 顶部标题 */}
+            {/*top title */}
             <div className="success-header">
-                <h1>Donation Submitted</h1>
+                <div className="success-checkmark">✓</div>
+                <h1>Donation Submitted!</h1>
                 <p>
                     Thank you so much for your support! We will process your donation soon 
                     and it will reflect in your donation histories!
                 </p>
+                
             </div>
 
-            {/* 分隔线 */}
-            <div className="divider"></div>
-
             
+            <div className="divider"></div>
         </div>
     );
 };
