fe548d1fe0945d0f216372af6141bd1053c0b81d
kaixuansoh <xuankaisoh@gmail.com>
2025-06-10 02:10:17 +0800

    Refactor admin

--- frontend/src/page/admin/pet/CreatePet.tsx
+++ frontend/src/page/admin/pet/CreatePet.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore, user_details } from '../../../store/auth.store';
 import '../../../css/shared/common.css';
-import '../../../css/createEditPet.css';
+import '../../../css/admin/pet.css';
 
 const CreatePet: React.FC = () => {
     const [formData, setFormData] = useState({
