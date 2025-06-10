23c888026aba2c357e7535b4ecfc472a57a5f099
kaixuansoh <xuankaisoh@gmail.com>
2025-06-11 06:37:43 +0800

    Enhance Donation and Appointment UI

--- frontend/src/page/donation/SuccessDonation.tsx
+++ frontend/src/page/donation/SuccessDonation.tsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
-import '../../css/donation/successDonation.css';
+import { Link } from 'react-router-dom';
+import '../../css/shared/success.css';
 
 const SuccessDonation = () => {
     return (
-        <div className="success-page">
-            {/*top title */}
+        <div className="success-container">
             <div className="success-header">
                 <div className="success-checkmark">✓</div>
                 <h1>Donation Submitted!</h1>
@@ -12,11 +11,15 @@ const SuccessDonation = () => {
                     Thank you so much for your support! We will process your donation soon 
                     and it will reflect in your donation histories!
                 </p>
-                
+                <div className="back-home-link-container">
+                    <Link
+                        to="/"
+                        className="back-home-link"
+                        >
+                        Back to Home
+                    </Link>
+                </div>
             </div>
-
-            
-            <div className="divider"></div>
         </div>
     );
 };
