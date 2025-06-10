bef3321fa997f2c40011f3afe1a23b85b39528e6
Kaixuan <178134705+kaixuansoh@users.noreply.github.com>
2025-06-11 04:58:49 +0800

    Enhance admin UI (#59)

--- frontend/src/component/Footer.tsx
+++ frontend/src/component/Footer.tsx
@@ -5,37 +5,37 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <div className="footer-column">
+                <div className="footer-column">
                 <h4>Adoption</h4>
                 <ul>
                     <li><Link to="#">Browse Listing.</Link></li>
                     <li><Link to="#">How To Adopt.</Link></li>
                     <li><Link to="#">FAQ.</Link></li>
                 </ul>
-            </div>
-            <div className="footer-column">
-                <h4>Support</h4>
-                <ul>
-                    <li><Link to="#">Donation</Link></li>
-                    <li><Link to="#">Be a Volunteer</Link></li>
-                </ul>
-            </div>
-            <div className="footer-column">
-                <h4>Adopme</h4>
-                <ul>
-                    <li><Link to="#">About Us</Link></li>
-                    <li><Link to="#">Campaigns</Link></li>
-                    <li><Link to="#">Send Us Feedback</Link></li>
-                </ul>
-            </div>
-            <div className="footer-column">
-                <h4>Contact Us</h4>
-                <ul>
-                    <li><Link to="#">adopme.shelter@gmail.com</Link></li>
-                    <li><Link to="#">+6012-3456789 (John)</Link></li>
-                    <li><Link to="#">1, Lorong AdopMe, Taman Adopme,<br></br>12345 Selangor, Malaysia</Link></li>
-                </ul>
-            </div>
+                </div>
+                <div className="footer-column">
+                    <h4>Support</h4>
+                    <ul>
+                        <li><Link to="#">Donation</Link></li>
+                        <li><Link to="#">Be a Volunteer</Link></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Adopme</h4>
+                    <ul>
+                        <li><Link to="#">About Us</Link></li>
+                        <li><Link to="#">Campaigns</Link></li>
+                        <li><Link to="#">Send Us Feedback</Link></li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Contact Us</h4>
+                    <ul>
+                        <li><Link to="#">adopme.shelter@gmail.com</Link></li>
+                        <li><Link to="#">+6012-3456789 (John)</Link></li>
+                        <li><Link to="#">1, Lorong AdopMe, Taman Adopme, 12345 Selangor, Malaysia</Link></li>
+                    </ul>
+                </div>
         </footer>
     );
 };
